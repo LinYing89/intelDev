ad14656b426aff5eab8ef2dd5ec4f94b0f91d71e
LinYing89 <444894216@qq.com>
2019-05-19 19:58:42 +0800

    default

--- src/main/java/com/bairock/iot/intelDev/data/DragConfig.java
+++ src/main/java/com/bairock/iot/intelDev/data/DragConfig.java
@@ -7,17 +7,22 @@ import javax.persistence.Id;
 @Entity
 public class DragConfig {
 
-    //组id
+    // 组id
     @Id
     private String devGroupId;
     // 组态页面背景图路径
     private String dragViewBackgroundImagePath = "";
+    // 组态视图背景图的宽度和高度
     @Column(columnDefinition = "int default 300", nullable = false)
     private int dragBackgroundWidth = 300;
     @Column(columnDefinition = "int default 300", nullable = false)
     private int dragBackgroundHeight = 300;
-    @Column(columnDefinition="boolean default true",nullable=false)
+    // 是否显示组态视图中设备的名称
+    @Column(columnDefinition = "boolean default true", nullable = false)
     private boolean showDeviceName = true;
+    // 是否显示组态视图中设备的图标
+    @Column(columnDefinition = "boolean default true", nullable = false)
+    private boolean showDeviceIcon = true;
 
     public String getDevGroupId() {
         return devGroupId;
@@ -57,5 +62,13 @@ public class DragConfig {
 
     public void setShowDeviceName(boolean showDeviceName) {
         this.showDeviceName = showDeviceName;
+    }
+
+    public boolean isShowDeviceIcon() {
+        return showDeviceIcon;
+    }
+
+    public void setShowDeviceIcon(boolean showDeviceIcon) {
+        this.showDeviceIcon = showDeviceIcon;
     }
 }
